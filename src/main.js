5e498d07d8ad789997d230dc9f77d9995de83aef
chentianyou <chentianyou@1dabang.cn>
2017-12-05 15:27:27 +0800

    1.安装插件：babel-plugin-component、babel-preset-es2015，支持部分引入 2.部分引入mint-ui

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import { Popup } from 'mint-ui'
 
 Vue.config.productionTip = false
 
+Vue.component(Popup.name, Popup)
 
 /* eslint-disable no-new */
 new Vue({

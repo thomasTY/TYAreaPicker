80a08b49855dddb22d20e69854fce15970a607c5
chentianyou <chentianyou@1dabang.cn>
2017-12-05 16:47:05 +0800

    引入 mint-ui 的popup、picker

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import { Popup } from 'mint-ui'
+import { Popup, Picker } from 'mint-ui'
 
 Vue.config.productionTip = false
 
 Vue.component(Popup.name, Popup)
+Vue.component(Picker.name, Picker)
 
 /* eslint-disable no-new */
 new Vue({
